b20c9f0bd5ad1e7a2e0dd97d779d37f0ac05f5c4
thanhcanhit <thanhcanh.dev@gmail.com>
2024-11-07 20:10:52 +0700

    Week 10 Todo app redux: ui

---
+++ Week10/pages/Home.js
@@ -0,0 +1,22 @@
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  View,
+  Image,
+  Button,
+  TextInput,
+} from 'react-native';
+import React, { PureComponent, useEffect, useState } from 'react';
+import { Checkbox } from 'react-native-paper';
+
+
+export default function App() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
